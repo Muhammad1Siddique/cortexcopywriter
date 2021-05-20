e38a181e2fc1f88256b80d4ae139dc31a0a1df95
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-20 20:24:49 +0500

    Some New Updates today

---
+++ src/Components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import './Header.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+}));
+
+export const Navbar = () =>{
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Switch Experience
+          </Typography>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
